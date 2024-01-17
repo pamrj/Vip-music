8a6aa5c9d4df18b9c19c4e1a84efeb243eb98933
pamrj <156561638+pamrj@users.noreply.github.com>
2024-01-17 18:23:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/cwteam169/CW-MUSIC",
+    "https://github.com/pamrj/Vip-music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
